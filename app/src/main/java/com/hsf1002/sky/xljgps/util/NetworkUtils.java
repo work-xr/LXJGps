2c411b80f3c06fb48a9e6ec7e7d7c5326f34b7ea
sky <hsf1002@gmail.com>
2018-09-12 10:20:14 +0800

    ping network

--- app/src/main/java/com/hsf1002/sky/xljgps/util/NetworkUtils.java
+++ app/src/main/java/com/hsf1002/sky/xljgps/util/NetworkUtils.java
@@ -7,6 +7,11 @@ import android.util.Log;
 
 import com.hsf1002.sky.xljgps.app.GpsApplication;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  * Created by hefeng on 18-8-27.
  */
@@ -25,5 +30,46 @@ public class NetworkUtils {
         Log.i(TAG, "isNetworkAvailable: " + isMobileConn);
 
         return isMobileConn;
+    }
+
+
+    /**
+    *  author:  hefeng
+    *  created: 18-9-11 下午8:11
+    *  desc:    网络能否ping通
+    *  param:
+    *  return:
+    */
+    public static final boolean ping() {
+
+        String result = null;
+        try {
+            String ip = "www.baidu.com";// ping 的地址，可以换成任何一种可靠的外网
+            Process p = Runtime.getRuntime().exec("ping -c 3 -w 100 " + ip);// ping网址3次
+            // 读取ping的内容，可以不加
+            InputStream input = p.getInputStream();
+            BufferedReader in = new BufferedReader(new InputStreamReader(input));
+            StringBuffer stringBuffer = new StringBuffer();
+            String content = "";
+            while ((content = in.readLine()) != null) {
+                stringBuffer.append(content);
+            }
+            Log.d("------ping-----", "result content : " + stringBuffer.toString());
+            // ping的状态
+            int status = p.waitFor();
+            if (status == 0) {
+                result = "success";
+                return true;
+            } else {
+                result = "failed";
+            }
+        } catch (IOException e) {
+            result = "IOException";
+        } catch (InterruptedException e) {
+            result = "InterruptedException";
+        } finally {
+            Log.d("----result---", "result = " + result);
+        }
+        return false;
     }
 }
